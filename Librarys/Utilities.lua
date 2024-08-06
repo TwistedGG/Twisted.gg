ee5fb60192ce8802504b5b184a380b56d01c9d89
CoolDude98 <165492777+TwistedGG@users.noreply.github.com>
2024-08-05 21:13:27 -0400

    Update and rename Utilities to Utilities.lua

--- Librarys/Utilities.lua
+++ Librarys/Utilities.lua
@@ -11,9 +11,6 @@ utils.onGround = function()
 	return Humanoid.FloorMaterial ~= Enum.Material.Air
 end
 utils.isMoving = function()
-	if onMobile then
-		return Humanoid.MoveDirection ~= Vector3.zero
-	end
 	return UserInputService:IsKeyDown("W") or UserInputService:IsKeyDown("A") or UserInputService:IsKeyDown("S") or UserInputService:IsKeyDown("D")
 end
 utils.newChat = function(Message)
