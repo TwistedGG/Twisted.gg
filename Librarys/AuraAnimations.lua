6b965b42a53cdfe0984887e482fabc9b6f7db327
CoolDude98 <165492777+TwistedGG@users.noreply.github.com>
2024-08-05 21:58:51 -0400

    Update AuraAnimations.lua

--- Librarys/AuraAnimations.lua
+++ Librarys/AuraAnimations.lua
@@ -1,28 +1,15 @@
 local Animations = {
-	["Normal"] = {
+	["Vanilla"] = {
+		{CFrame = CFrame.new(0.7, -0.3, 0.612) * CFrame.Angles(math.rad(285), math.rad(65), math.rad(293)), Timer = 0.2},
+		{CFrame = CFrame.new(0.61, -0.41, 0.6) * CFrame.Angles(math.rad(210), math.rad(70), math.rad(3)), Timer = 0.2},
+	},
+	["Swang"] = {
 		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-65), math.rad(55), math.rad(-51)), Timer = 0.1},
 		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-179), math.rad(94), math.rad(33)), Timer = 0.16},
-	},
-	["Spin"] = {
-		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-90), math.rad(0), math.rad(0)), Timer = 0.05},
-		{CFrame = CFrame.new(0.69, -0.7, 0.1) * CFrame.Angles(math.rad(-145), math.rad(0), math.rad(0)), Timer = 0.05},
-		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-180), math.rad(0), math.rad(0)), Timer = 0.05},
-		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-220), math.rad(0), math.rad(0)), Timer = 0.05},
-		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-270), math.rad(0), math.rad(0)), Timer = 0.05},
-		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-310), math.rad(0), math.rad(0)), Timer = 0.05},
-		{CFrame = CFrame.new(0.16, -1.16, 0.5) * CFrame.Angles(math.rad(-360), math.rad(0), math.rad(0)), Timer = 0.05},
 	},
 	["Sigma"] = {
 		{CFrame = CFrame.new(-0.69, -0.7, 0.6) * CFrame.Angles(math.rad(-10), math.rad(50), math.rad(-90)), Timer = 0.1},
 		{CFrame = CFrame.new(-0.7, -0.71, 0.59) * CFrame.Angles(math.rad(-64), math.rad(70), math.rad(-38)), Timer = 0.2}
-	},
-	["Moon"] = {
-		{CFrame = CFrame.new(0.7, -2, 0.612) * CFrame.Angles(math.rad(296), math.rad(61), math.rad(273)), Timer = 0.2},
-		{CFrame = CFrame.new(0.61, 0.41, 0.6) * CFrame.Angles(math.rad(210), math.rad(74), math.rad(8)), Timer = 0.2},
-	},
-	["Clean"] = {
-		{CFrame = CFrame.new(0.7, -0.3, 0.612) * CFrame.Angles(math.rad(285), math.rad(65), math.rad(293)), Timer = 0.2},
-		{CFrame = CFrame.new(0.61, -0.41, 0.6) * CFrame.Angles(math.rad(210), math.rad(70), math.rad(3)), Timer = 0.2},
 	},
 }
 return Animations
